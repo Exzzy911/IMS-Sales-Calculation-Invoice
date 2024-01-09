5b72961e375c032bcb2b3dfa182c4c995f52d3c5
Exzzy911 <141855192+Exzzy911@users.noreply.github.com>
2024-01-09 13:46:52 +0545

    First Commit

---
+++ index.js
@@ -0,0 +1,34 @@
+let val;
+let val1;
+let val2;
+let val3;
+let val4;
+ 
+
+document.getElementById("myButton").onclick = function(){
+
+    
+    val = document.getElementById("myText").value;
+    console.log(val);
+    val1 = document.getElementById("myText1").value;
+    console.log(val1);
+    val2 = document.getElementById("myText2").value;
+    console.log(val2);
+    val3 = document.getElementById("myText3").value;
+    console.log(val3);
+    val4 = document.getElementById("myText4").value;
+    console.log(val4);
+
+    let profit = (val2 - val1) * val4;
+    document.getElementById("myText5").value = profit;
+
+
+    document.getElementById("myTexti").innerText = val;
+    document.getElementById("myTextii").innerText = val1;
+    document.getElementById("myTextiii").innerText = val2;
+    document.getElementById("myTextiv").innerText = val3;
+    document.getElementById("myTextv").innerText = val4;
+    document.getElementById("myTextvi").innerText = profit;
+     
+}
+
